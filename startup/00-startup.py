d7f05cb2d5a00226a4ba23cf32ac1c4f32169279
Dan Allan <dallan@bnl.gov>
2016-05-11 12:24:26 -0400

    Update beamline config after software upgrade (bluesky v0.5.1 etc)

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,53 +1,45 @@
-import logging
-
-from ophyd.commands import *
-
-
-from databroker import DataBroker as db, get_events, get_images, get_table
-
-import bluesky.qt_kicker  # make matplotlib qt backend play nice with bluesky asyncio
-
-import asyncio
-from functools import partial
-from functools import partial
-from bluesky.standard_config import *
-from bluesky.global_state import abort, stop, resume, all_is_well, panic
-from bluesky.plans import *
-from bluesky.callbacks import *
-from bluesky.broker_callbacks import *
-from bluesky.scientific_callbacks import plot_peak_stats
-from bluesky import qt_kicker   # provides the libraries for live plotting
-qt_kicker.install_qt_kicker()    # installs the live plotting libraries
+# Make ophyd listen to pyepics.
+from ophyd import setup_ophyd
 setup_ophyd()
 
-# Set up default metadata.
-gs.RE.md['group'] = 'iss'
-gs.RE.md['beamline_id'] = 'ISS'
+# Subscribe metadatastore to documents.
+# If this is removed, data is not saved to metadatastore.
+import metadatastore.commands
+from bluesky.global_state import gs
+gs.RE.subscribe_lossless('all', metadatastore.commands.insert)
 
-
-# alias
-RE = gs.RE
-
-
-# Set up the logbook.
-LOGBOOKS = ['Data Acquisition']
-import bluesky.callbacks.olog
-from pyOlog import SimpleOlogClient
-simple_olog_client = SimpleOlogClient()
-generic_logbook_func = simple_olog_client.log
-configured_logbook_func = partial(generic_logbook_func, logbooks=LOGBOOKS)
-
-from bluesky.callbacks.olog import logbook_cb_factory
-cb = logbook_cb_factory(configured_logbook_func)
-# RE.subscribe('start', cb)
-
-logbook = simple_olog_client  # this is for ophyd.commands.get_logbook
-
-# Turn off "noisy" debugging.
-loop = asyncio.get_event_loop()
-loop.set_debug(False)
+# At the end of every run, verify that files were saved and
+# print a confirmation message.
+from bluesky.callbacks.broker import verify_files_saved
+gs.RE.subscribe('stop', post_run(verify_files_saved))
 
 # Import matplotlib and put it in interactive mode.
 import matplotlib.pyplot as plt
 plt.ion()
 
+# Make plots update live while scans run.
+from bluesky.utils import install_qt_kicker
+install_qt_kicker()
+
+# Optional: set any metadata that rarely changes.
+# RE.md['beamline_id'] = 'YOUR_BEAMLINE_HERE'
+
+# convenience imports
+from ophyd.commands import *
+from bluesky.callbacks import *
+from bluesky.spec_api import *
+from bluesky.global_state import gs, abort, stop, resume
+from databroker import (DataBroker as db, get_events, get_images,
+                        get_table, get_fields, restream, process)
+from time import sleep
+import numpy as np
+
+RE = gs.RE  # convenience alias
+
+# Uncomment the following lines to turn on verbose messages for debugging.
+# import logging
+# ophyd.logger.setLevel(logging.DEBUG)
+
+# Set up default metadata.
+gs.RE.md['group'] = 'iss'
+gs.RE.md['beamline_id'] = 'ISS'
