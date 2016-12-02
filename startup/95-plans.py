c7425380ddfb91656e4e829ea1989e27887f8bf5
Bruno Luvizotto <luvizotto@bnl.gov>
2016-12-02 10:21:17 -0500

    Added the trajectory filename to the scan metadata stored

--- startup/95-plans.py
+++ startup/95-plans.py
@@ -148,7 +148,7 @@ def prep_traj_plan(delay = 0.25):
 def execute_trajectory(comment, **metadata):
     flyers = [pb9.enc1, pba1.adc1, pba2.adc6, pba2.adc7]
     def inner():
-        md = {'plan_args': {}, 'plan_name': 'execute_trajectory','experiment': 'transmission', 'comment': comment, pba1.adc1.name + ' offset': pba1.adc1.offset.value, pba2.adc6.name + ' offset': pba2.adc6.offset.value, pba2.adc7.name + ' offset': pba2.adc7.offset.value}
+        md = {'plan_args': {}, 'plan_name': 'execute_trajectory','experiment': 'transmission', 'comment': comment, pba1.adc1.name + ' offset': pba1.adc1.offset.value, pba2.adc6.name + ' offset': pba2.adc6.offset.value, pba2.adc7.name + ' offset': pba2.adc7.offset.value, 'trajectory_name': hhm.trajectory_name.value}
         md.update(**metadata)
         yield from bp.open_run(md=md)
 
@@ -216,7 +216,7 @@ def execute_xia_trajectory(comment, **metadata):
         xia1.netcdf_filename.put(comment) #yield from bp.abs_set(xia1.netcdf_filename, comment, wait=True)#
         next_file_number = xia1.netcdf_filenumber_rb.value #(yield from bp.read(xia1.netcdf_filenumber_rb))#
 
-        md = {'plan_args': {}, 'plan_name': 'execute_xia_trajectory','experiment': 'fluorescence_sdd', 'comment': comment, 'xia_filename': '{}_{:03}.nc'.format(comment, next_file_number), pba1.adc1.name + ' offset': pba1.adc1.offset.value, pba2.adc6.name + ' offset': pba2.adc6.offset.value, pba2.adc7.name + ' offset': pba2.adc7.offset.value}
+        md = {'plan_args': {}, 'plan_name': 'execute_xia_trajectory','experiment': 'fluorescence_sdd', 'comment': comment, 'xia_filename': '{}_{:03}.nc'.format(comment, next_file_number), pba1.adc1.name + ' offset': pba1.adc1.offset.value, pba2.adc6.name + ' offset': pba2.adc6.offset.value, pba2.adc7.name + ' offset': pba2.adc7.offset.value, 'trajectory_name': hhm.trajectory_name.value}
         md.update(**metadata)
         yield from bp.open_run(md=md)
 
@@ -289,7 +289,7 @@ def execute_loop_trajectory(comment, **metadata):
 
     flyers = [pb9.enc1, pba1.adc1, pba2.adc6, pba2.adc7]
     def inner():
-        md = {'plan_args': {}, 'plan_name': 'execute_loop_trajectory','experiment': 'transmission', 'comment': comment, pba1.adc1.name + ' offset': pba1.adc1.offset.value, pba2.adc6.name + ' offset': pba2.adc6.offset.value, pba2.adc7.name + ' offset': pba2.adc7.offset.value}
+        md = {'plan_args': {}, 'plan_name': 'execute_loop_trajectory','experiment': 'transmission', 'comment': comment, pba1.adc1.name + ' offset': pba1.adc1.offset.value, pba2.adc6.name + ' offset': pba2.adc6.offset.value, pba2.adc7.name + ' offset': pba2.adc7.offset.value, 'trajectory_name': hhm.trajectory_name.value}
         md.update(**metadata)
         yield from bp.open_run(md=md)
 
