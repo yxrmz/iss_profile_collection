5fc71d764b41f3309217b2931bc8d7b8196268d1
elistavitski <elistavitski@gmail.com>
2019-03-07 14:00:01 -0500

    Corrected spiral scans

--- startup/91-spiral_plans.py
+++ startup/91-spiral_plans.py
@@ -7,15 +7,14 @@ def find_holder_fiducial():
     motor_x = motor_dictionary['giantxy_x']['object']
     motor_y = motor_dictionary['giantxy_y']['object']
 
-    plan = rel_spiral_square([detector],motor_x,motor_y,5,5,10,10)
+    plan = rel_spiral_square([detector],motor_x,motor_y,5,5,20,20)
 
     if hasattr(detector, 'kickoff'):
         plan_with_flyers = bpp.fly_during_wrapper(plan, [detector])
     uid = (yield from plan_with_flyers)
-    table = db[uid].table()
-    row_num = table[detector.volt.name].idxmin()
-    x_pos = table['giantxy_x'][row_num]
-    y_pos = table['giantxy_y'][row_num]
-    return x_pos, y_pos
+    # table = db[uid].table()
+    # row_num = table[detector.volt.name].idxmin()
+    # x_pos = table['giantxy_x'][row_num]
+    # y_pos = table['giantxy_y'][row_num]
 
 
