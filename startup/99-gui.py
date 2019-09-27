ccf614c7e6568aa262e890b0a3a297a10422709f
elistavitski <elistavitski@gmail.com>
2019-09-27 16:09:51 -0400

    Add fly scan with EM

--- startup/99-gui.py
+++ startup/99-gui.py
@@ -12,6 +12,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 xlive_gui = xlive.XliveGui(plan_funcs={
                                     'Fly scan':                     fly_scan,
+                                    'Fly scan with EM' :            fly_scan_with_em,
                                     'Step scan':                    step_scan,
                                     'Constant energy' :             constant_energy,
                                     'Spiral fly scan':              fly_scan_over_spiral,
