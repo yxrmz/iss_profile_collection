aba80d3c86ed194db7ca4e79c1e7432723a5b216
elistavitski <elistavitski@gmail.com>
2019-11-15 17:02:15 -0500

    Change handler to add binary data with embedded datastamp

--- startup/30-electrometer.py
+++ startup/30-electrometer.py
@@ -166,7 +166,7 @@ class ElectrometerBinFileHandler(HandlerBase):
             fp.readline()
             Ranges = [int(x) for x in fp.readline().split(':')[1].split(',')]
             FArate = float(fp.readline().split(':')[1])
-            trigger_timestamp = float(fp.readline().split(':')[1].strip().replace(',', '.'))
+            # trigger_timestamp = float(fp.readline().split(':')[1].strip().replace(',', '.'))
 
             def Range(val):
                 ranges = {1: 1,
@@ -181,9 +181,8 @@ class ElectrometerBinFileHandler(HandlerBase):
                 return ret
 
             # 1566332720 366808768 -4197857 11013120 00
-            self.raw_data = X = np.fromfile(fpath, dtype=np.int32)
+            self.raw_data = raw_data = np.fromfile(fpath, dtype=np.int32)
 
-            print(len(X))
             A = []
             B = []
             C = []
@@ -193,22 +192,32 @@ class ElectrometerBinFileHandler(HandlerBase):
             Rb = Range(Ranges[1])
             Rc = Range(Ranges[2])
             Rd = Range(Ranges[3])
-            dt = 1.0 / FArate / 1000.0
+            # dt = 1.0 / FArate / 1000.0
 
-            for i in range(0, len(X), 4):
-                A.append(Ra * ((X[i] / FAdiv) - Offsets[0]) / Gains[0])
-                B.append(Rb * ((X[i + 1] / FAdiv) - Offsets[1]) / Gains[1])
-                C.append(Rc * ((X[i + 2] / FAdiv) - Offsets[2]) / Gains[2])
-                D.append(Rd * ((X[i + 3] / FAdiv) - Offsets[3]) / Gains[3])
-                T.append(i * dt / 4.0)
+            num_columns = 6
+            raw_data = raw_data.reshape((raw_data.size // num_columns, num_columns))
 
-        data = np.vstack((np.array(T) + trigger_timestamp,
-                          np.array(A),
-                          np.array(B),
-                          np.array(C),
-                          np.array(D))).T
+            derived_data = np.zeros((raw_data.shape[0], raw_data.shape[1] - 1))
+            derived_data[:, 0] = raw_data[:, -2] + raw_data[:, -1] * 8.0051232 * 1e-9  # Unix timestamp with nanoseconds
+            derived_data[:, 1] = Ra * ((raw_data[:, 0] / FAdiv) - Offsets[0]) / Gains[0]
+            derived_data[:, 2] = Ra * ((raw_data[:, 1] / FAdiv) - Offsets[1]) / Gains[1]
+            derived_data[:, 3] = Ra * ((raw_data[:, 2] / FAdiv) - Offsets[2]) / Gains[2]
+            derived_data[:, 4] = Ra * ((raw_data[:, 3] / FAdiv) - Offsets[3]) / Gains[3]
 
-        self.df = pd.DataFrame(data=data, columns=['timestamp', 'i0', 'it', 'ir', 'iff'])
+            # for i in range(0, len(X), 4):
+            #     A.append(Ra * ((X[i + 0] / FAdiv) - Offsets[0]) / Gains[0])
+            #     B.append(Rb * ((X[i + 1] / FAdiv) - Offsets[1]) / Gains[1])
+            #     C.append(Rc * ((X[i + 2] / FAdiv) - Offsets[2]) / Gains[2])
+            #     D.append(Rd * ((X[i + 3] / FAdiv) - Offsets[3]) / Gains[3])
+            #     T.append(i * dt / 4.0)
+
+        # data = np.vstack((np.array(T) + trigger_timestamp,
+        #                   np.array(A),
+        #                   np.array(B),
+        #                   np.array(C),
+        #                   np.array(D))).T
+
+        self.df = pd.DataFrame(data=derived_data, columns=['timestamp', 'i0', 'it', 'ir', 'iff'])
 
     def __call__(self):
         return self.df, self.raw_data
