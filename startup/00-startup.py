0376ff2ae4aa2fec5f1b0327b0a45e81f4bff974
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-03 14:07:56 -0400

    Clean up 00-startup & add PersistentDict class

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,82 +1,95 @@
-import time
-import sys
-from bluesky.simulators import summarize_plan
-
-# Check version of bluesky and act accordingly
-from distutils.version import LooseVersion
-import bluesky
-if bluesky.__version__ < LooseVersion('1.6'):
-    OLD_BLUESKY = True
-else:
-    OLD_BLUESKY = False
-
-# Set up a RunEngine and use metadata backed by a sqlite file.
 print(__file__)
-from bluesky import RunEngine
 
-from bluesky.utils import get_history
-RE = RunEngine({})
-
-# Set up a Broker.
-from databroker import Broker
-db = Broker.named('iss')
-db_analysis = Broker.named('iss-analysis')
-
-# Subscribe metadatastore to documents.
-# If this is removed, data is not saved to metadatastore.
-RE.subscribe(db.insert)
-
-# Set up SupplementalData.
-from bluesky import SupplementalData
-sd = SupplementalData()
-RE.preprocessors.append(sd)
-
-# Add a progress bar.
+import logging
+import sys
+import time
+from pathlib import Path
 from timeit import default_timer as timer
 
+import appdirs
+import bluesky
+import nslsii
+from bluesky.simulators import summarize_plan
 
-from bluesky.utils import ProgressBarManager
-pbar_manager = ProgressBarManager()
-#RE.waiting_hook = pbar_manager
+nslsii.configure_base(get_ipython().user_ns, 'iss')
 
-# Register bluesky IPython magics.
-from bluesky.magics import BlueskyMagics
-get_ipython().register_magics(BlueskyMagics)
+# Temporary fix before it's fixed in ophyd
+logger = logging.getLogger('ophyd')
+logger.setLevel('WARNING')
 
-# Set up the BestEffortCallback.
-from bluesky.callbacks.best_effort import BestEffortCallback
-bec = BestEffortCallback()
+
 bec.disable_plots()
 bec.disable_table()
 RE.subscribe(bec)
 peaks = bec.peaks  # just as alias for less typing
 
+
+
+
+try:
+    from bluesky.utils import PersistentDict
+except ImportError:
+    import msgpack
+    import msgpack_numpy
+    import zict
+
+    class PersistentDict(zict.Func):
+        def __init__(self, directory):
+            self._directory = directory
+            self._file = zict.File(directory)
+            super().__init__(self._dump, self._load, self._file)
+
+        @property
+        def directory(self):
+            return self._directory
+
+        def __repr__(self):
+            return f"<{self.__class__.__name__} {dict(self)!r}>"
+
+        @staticmethod
+        def _dump(obj):
+            "Encode as msgpack using numpy-aware encoder."
+            # See https://github.com/msgpack/msgpack-python#string-and-binary-type
+            # for more on use_bin_type.
+            return msgpack.packb(
+                obj,
+                default=msgpack_numpy.encode,
+                use_bin_type=True)
+
+        @staticmethod
+        def _load(file):
+            return msgpack.unpackb(
+                file,
+                object_hook=msgpack_numpy.decode,
+                raw=False)
+
+runengine_metadata_dir = Path('/nsls2/xf08id/metadata/') / Path("runengine-metadata")
+
+# PersistentDict will create the directory if it does not exist
+RE.md = PersistentDict(runengine_metadata_dir)
+
+# Check version of bluesky and act accordingly
+from distutils.version import LooseVersion
+
+if bluesky.__version__ < LooseVersion('1.6'):
+    OLD_BLUESKY = True
+else:
+    OLD_BLUESKY = False
+
+
 if OLD_BLUESKY:
     # Make plots update live while scans run.
     from bluesky.utils import install_qt_kicker
     install_qt_kicker()
-
-    from pathlib import Path
-    from historydict import HistoryDict
-
-    try:
-        RE.md = HistoryDict('/nsls2/xf08id/metadata/bluesky_history.db')
-        print('gpfs')
-    except Exception as exc:
-        print('local')
-        print(exc)
-        RE.md = HistoryDict(str(Path('~/.config/bluesky/bluesky_history.db'.).expanduser()))
 else:
     # Patch to fix Tom's terrible deeds
     import matplotlib.backends.backend_qt5
-    from matplotlib.backends.backend_qt5 import _create_qApp
     from matplotlib._pylab_helpers import Gcf
+    from matplotlib.backends.backend_qt5 import _create_qApp
 
     _create_qApp()
     qApp = matplotlib.backends.backend_qt5.qApp
 
-    from bluesky.utils import PersistentDict
-    RE.md = PersistentDict('/nsls2/xf08id/metadata/bluesky-md')
 
 RE.is_aborted = False
 
