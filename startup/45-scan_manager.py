6948b03d0b43a884f79eb0fa619380914d6ea886
dleshchev <leshchev.denis@gmail.com>
2021-11-15 16:47:21 -0500

    scan manager WIP addition/deletion of scans in local/global managers are operational now

--- startup/45-scan_manager.py
+++ startup/45-scan_manager.py
@@ -1,32 +1,55 @@
 
 import json
 import uuid
+
+import numpy as np
 from PyQt5 import QtGui
+from xas.trajectory import TrajectoryCreator
+from xas.xray import generate_energy_grid_from_dict
+import os
+
+
+
+
+
 
 class ScanManager():
     def __init__(self, scan_manager_json = '/nsls2/xf08id/settings/json/scan_manager.json'):
-        with open(scan_manager_json, 'r') as f:
-            self.scan_dict= json.loads(f.read())
+        self.json_file_path = scan_manager_json
+        with open(self.json_file_path, 'r') as f:
+            self.scan_dict = json.loads(f.read())
 
+        self.json_file_path_local = None
+        self.scan_list_local = None
 
+        self.trajectory_creator = TrajectoryCreator()
 
+    def init_local_manager(self):
+        self.json_file_path_local = f"{ROOT_PATH}/{USER_FILEPATH}/{RE.md['year']}/{RE.md['cycle']}/{RE.md['PROPOSAL']}/scan_manager.json"
+        self.scan_list_local = []
 
+    def dump_local_scan_list(self):
+        with open(self.json_file_path_local, 'w') as f:
+            json.dump(self.scan_list_local, f )
 
+    def add_scan(self, scan, aux_parameters, name, model= None):
+        uid = self.check_if_brand_new(scan)
+        scan_def = name + ' ' + scan['scan_type'] + ' at ' + scan['scan_parameters']['element'] + \
+                   ' ' + scan['scan_parameters']['edge'] + ' edge'
+        if self.scan_list_local is None:
+            self.init_local_manager()
+        scan_local = {'uid' : uid, 'scan_def' : scan_def, 'aux_parameters' : aux_parameters}
+        self.scan_list_local.append(scan_local)
+        self.dump_local_scan_list()
+        return uid
 
-    def add_scan(self, new_scan, name, model= None):
-        uid = self.check_if_brand_new(new_scan)
-        if uid:
-            item = QtGui.QStandardItem(name)
-            item.uid = uid
-            if model:
-                parent = model.invisibleRootItem()
-                parent.appendRow(item)
-
-
+    def delete_local_scan(self, idx):
+        self.scan_list_local.pop(idx)
+        self.dump_local_scan_list()
 
     def check_if_brand_new(self, new_scan):
         for uid, scan in self.scan_dict.items():
-            if scan['scan_type'] == new_scan['scan_type']:
+            if scan['scan_type'] == scan['scan_type']:
                 scan_parameters = scan['scan_parameters']
                 new_scan_parameters = new_scan['scan_parameters']
                 keys =  scan_parameters.keys()
@@ -34,34 +57,65 @@ class ScanManager():
                 if all([(new_k in keys) for new_k in new_keys]):
                     parameters_match = True
                     for k in keys:
-                        print('>>>>>>>>>>>>>>>>>>>>>>>', k)
-                        if (k != 'filename') and (k != 'offset'):
+                        if k != 'filename':
                             if scan_parameters[k] != new_scan_parameters[k]:
-                                print(k)
                                 parameters_match = False
                                 break
-
                     if parameters_match:
-                        dets = scan['detectors']
-                        new_dets = new_scan['detectors']
-                        if all([d in new_dets for d in dets]):
-                            print('SCAN IS OLD')
-                            return uid
-                        else:
-                            new_scan['scan_parameters']['filename'] = scan['scan_parameters']['filename']
-                            break
+                        print('SCAN IS OLD')
+                        return uid
 
         new_uid = self.make_scan_uid()
-        # new_scan['uid'] = new_uid
         print('SCAN IS NEW')
         self.scan_dict[new_uid] = new_scan
+        self.create_trajectory_file(new_scan, new_uid)
+        os.rename(self.json_file_path, f'{os.path.splitext(self.json_file_path)[0]}.bak')
+        with open(self.json_file_path, 'w') as f:
+            json.dump(self.scan_dict, f )
         return new_uid
+
 
     def make_scan_uid(self):
         return str(uuid.uuid4())[:13]
 
 
+    def create_lightweight_trajectory(self, scan, plot_func):
+        if scan['scan_type'] == 'fly scan':
+            self.trajectory_creator.define_complete(scan['scan_parameters'], lightweight=True)
+            energy = self.trajectory_creator.energy
+            time = self.trajectory_creator.time
+        elif scan['scan_type'] == 'step scan':
+            energy, _, time = generate_energy_grid_from_dict(scan['scan_parameters'])
+        plot_func(time, energy)
+
+
+    def create_trajectory_file(self, scan, uid):
+        filename = f'{uid}.txt'
+        filepath = trajectory_manager.trajectory_path + filename
+        if scan['scan_type'] == 'fly scan':
+            self.trajectory_creator.define_complete(scan['scan_parameters'])
+            self.trajectory_creator.save(filepath)
+        elif scan['scan_type'] == 'step scan':
+            energy, dwell_time, _ = generate_energy_grid_from_dict(scan['scan_parameters'])
+            data = np.vstack((energy, dwell_time)).T
+            element = scan['scan_parameters']['element']
+            edge = scan['scan_parameters']['edge']
+            e0 = scan['scan_parameters']['e0']
+            if scan['scan_parameters']['revert'] : direction = 'backward'
+            else: direction = 'forward'
+            header = f'element: {element}, edge: {edge}, E0: {e0}, direction: {direction}'
+            np.savetxt(filepath, data, header=header)
+
+        self.scan_dict[uid]['scan_parameters']['filename'] = filename
+
+
+
+
 scan_manager = ScanManager()
+
+
+
+
 
 
 
