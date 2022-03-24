8f5c782e3604dbaf86f3f93554f61033afa7dfcc
dleshchev leshchev.denis@gmail.com <leshchev.denis@gmail.com>
2022-03-24 11:54:04 -0400

    fixed amp reference in the metadata added get_hook function fixed "skip" action when adding plans

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -287,9 +287,11 @@ stop = timer()
 
 print("MD handling complete in {} sec".format(stop - start))
 
-from bluesky.utils import ts_msg_hook
-RE.msg_hook = ts_msg_hook
+def get_hook():
+    from bluesky.utils import ts_msg_hook
+    RE.msg_hook = ts_msg_hook
 
+# get_hook()
 
 # the file paths for acquitision and analysis
 ROOT_PATH = '/nsls2/xf08id'
