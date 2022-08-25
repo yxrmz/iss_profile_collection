02496644ea491671a5f48c1a33e9efee4c9a99ff
dleshchev leshchev.denis@gmail.com <leshchev.denis@gmail.com>
2022-08-25 15:54:16 -0400

    unified metadata generation

--- startup/60-plan_metadata.py
+++ startup/60-plan_metadata.py
@@ -1,77 +1,127 @@
 from xas.file_io import validate_file_exists
-from xas.metadata import populate_standard_metadata_dict, get_standard_metadata
-populate_standard_metadata_dict()
-# populate_md_dict({'nslsii_status':                  nsls_ii.return_status_string,
-#                   'nslsii_current':                 nsls_ii.beam_current,
-#                   'nslsii_energy':                  nsls_ii.get_energy_str,
-#                   'beamline_cm1':                   cm1.get_current_stripe,
-#                   'beamline_cm2':                   cm2.get_current_stripe,
-#                   'beamline_cm2_bender':            bender.load_cell,
-#                   'beamline_fm':                    fm.get_current_stripe,
-#                   'beamline_fm_bender':             bender_fm.load_cell,
-#                   'beamline_harmonic_rejection':    hhrm.get_current_stripe,
-#                   'angle_offset' :                  hhm.angle_offset,
-#                   'angle_offset_deg' :              hhm.get_angle_offset_deg_str,
-#                   'mono_encoder_resolution':        hhm.get_mono_encoder_resolution_str,
-#                   'detector_i0_n2': (lambda : f'{np.round(gas_n2.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
-#                   'detector_it_n2': (lambda : f'{np.round(gas_n2.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
-#                   'detector_ir_n2': (lambda : f'{np.round(gas_n2.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
-#                   'detector_i0_he': (lambda : f'{np.round(gas_he.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
-#                   'detector_it_he': (lambda : f'{np.round(gas_he.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
-#                   'detector_ir_he': (lambda : f'{np.round(gas_he.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
-#                   'detector_i0_volt': wps1.hv302.read_pv,
-#                   'detector_it_volt': wps1.hv303.read_pv,
-#                   'detector_ir_volt': wps1.hv305.read_pv,
-#                   'i0_gain': i0_amp.get_gain_value,
-#                   'it_gain': it_amp.get_gain_value,
-#                   'ir_gain': ir_amp.get_gain_value,
-#                   'iff_gain': iff_amp.get_gain_value,
-#                   'sample_x_position':  sample_stage.x.user_readback,
-#                   'sample_y_position':  sample_stage.y.user_readback,
-#                   'sample_z_position':  sample_stage.z.user_readback,
-#                   'sample_th_position': sample_stage.th.user_readback,
-# })
+from xas.metadata import metadata_dict
+
+def populate_standard_metadata_dict(obj_dict=None):
+    if obj_dict is None:
+        obj_dict = {}
+
+    for key, item in metadata_dict.items():
+        if item['kind'] == 'attribute':
+            if key in obj_dict.keys():
+                item['object'] = obj_dict[key]
+            else:
+                item['object'] = None
+        elif item['kind'] == 'epics_pv':
+            if 'pv_str' in item.keys():
+                item['object'] = EpicsSignalRO(item['pv_str'], name=key)
+            else:
+
+                if key in obj_dict.keys():
+                    item['object'] = obj_dict[key]
+                else:
+                    item['object'] = None
+
+populate_standard_metadata_dict({
+'nslsii_status':                  nsls_ii.return_status_string,
+'nslsii_current':                 nsls_ii.beam_current,
+'nslsii_energy':                  nsls_ii.get_energy_str,
+'beamline_cm1':                   cm1.get_current_stripe,
+'beamline_cm2':                   cm2.get_current_stripe,
+'beamline_cm2_bender':            bender.load_cell,
+'beamline_fm':                    fm.get_current_stripe,
+'beamline_fm_bender':             bender_fm.load_cell,
+'beamline_harmonic_rejection':    hhrm.get_current_stripe,
+'angle_offset' :                  hhm.angle_offset,
+'angle_offset_deg' :              hhm.get_angle_offset_deg_str,
+'mono_encoder_resolution':        hhm.get_mono_encoder_resolution_str,
+'detector_i0_n2':                (lambda : f'{np.round(gas_n2.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
+'detector_it_n2':                (lambda : f'{np.round(gas_n2.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
+'detector_ir_n2':                (lambda : f'{np.round(gas_n2.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
+'detector_i0_he':                (lambda : f'{np.round(gas_he.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
+'detector_it_he':                (lambda : f'{np.round(gas_he.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
+'detector_ir_he':                (lambda : f'{np.round(gas_he.flow.get() / (gas_he.flow.get() + gas_n2.flow.get()) * 100)}%'),
+'detector_i0_volt':              wps1.hv302.read_pv,
+'detector_it_volt':              wps1.hv303.read_pv,
+'detector_ir_volt':              wps1.hv305.read_pv,
+'i0_gain':                       i0_amp.get_gain_value,
+'it_gain':                       it_amp.get_gain_value,
+'ir_gain':                       ir_amp.get_gain_value,
+'iff_gain':                      iff_amp.get_gain_value,
+'sample_x_position':             sample_stage.x.user_readback,
+'sample_y_position':             sample_stage.y.user_readback,
+'sample_z_position':             sample_stage.z.user_readback,
+'sample_th_position':            sample_stage.th.user_readback,
+})
+
+
+def get_standard_metadata():
+    md = {}
+    for key, item in metadata_dict.items():
+        if item['kind'] == 'auto':
+            continue
+
+        elif item['kind'] == 'attribute':
+            object = item['object']
+            if object is not None:
+                value = item['object']()
+            else:
+                value = ''
+
+        elif item['kind'] == 'epics_pv':
+            object = item['object']
+            if object is not None:
+                value = item['object'].get()
+            else:
+                value = ''
+
+        elif item['kind'] == 'fixed_value':
+            value = item['value']
+
+        md[key] = value
+
+    return md
 
 
 def get_general_md():
-    gas_n2_flow = gas_n2.flow.get()
-    gas_he_flow = gas_he.flow.get()
-    gas_tot_flow = gas_n2_flow + gas_he_flow
-    gas_he_perc = np.round(gas_he_flow / gas_tot_flow * 100)
-    gas_n2_perc = np.round(gas_n2_flow / gas_tot_flow * 100)
-
-    i0_volt = np.round(wps1.hv302.read_pv.get())
-    it_volt = np.round(wps1.hv303.read_pv.get())
-    ir_volt = np.round(wps1.hv305.read_pv.get())
-    # print_to_gui('WARNING VOLTAGES ARE NOT BEING READ FOR METADATA')
-    # i0_volt = 0
-    # it_volt = 0
-    # ir_volt = 0
-
-    md = {'nslsii_current' : nsls_ii.beam_current.get(),
-          'nslsii_status' : nsls_ii.return_status_string(),
-          'nslsii_energy' : nsls_ii.energy_str,
-          'pulses_per_degree': hhm.pulses_per_deg,
-          'angle_offset': str(hhm.angle_offset.get()),
-          'angle_offset_deg': f'{np.round(hhm.angle_offset.get() * 180 / np.pi, 3)} deg',
-          'mono_encoder_resolution': str(np.round(hhm.main_motor_res.get() * np.pi / 180 * 1e9)) + ' nrad',
-          'mono_scan_mode': 'pseudo-channel cut',
-          'harmonic_rejection' : hhrm.current_stripe,
-          'sample_stage': 'ISS.giant_xy stage',
-          'sample_x_position': giantxy.x.user_readback.get(),
-          'sample_y_position': giantxy.y.user_readback.get(),
-          'i0_par' : f'17 cm, He: {gas_he_perc}%, N2: {gas_n2_perc}%',
-          'it_par' : f'30 cm, He: {gas_he_perc}%, N2: {gas_n2_perc}%',
-          'ir_par' : f'17 cm, He: {gas_he_perc}%, N2: {gas_n2_perc}%',
-          'iff_par' : f'PIPS',
-          'i0_volt' : i0_volt,
-          'it_volt' : it_volt,
-          'ir_volt' : ir_volt,
-          'i0_gain' : i0_amp.get_gain()[0],
-          'it_gain' : it_amp.get_gain()[0],
-          'ir_gain' : ir_amp.get_gain()[0],
-          'iff_gain' : iff_amp.get_gain()[0],
-          }
+    # gas_n2_flow = gas_n2.flow.get()
+    # gas_he_flow = gas_he.flow.get()
+    # gas_tot_flow = gas_n2_flow + gas_he_flow
+    # gas_he_perc = np.round(gas_he_flow / gas_tot_flow * 100)
+    # gas_n2_perc = np.round(gas_n2_flow / gas_tot_flow * 100)
+    #
+    # i0_volt = np.round(wps1.hv302.read_pv.get())
+    # it_volt = np.round(wps1.hv303.read_pv.get())
+    # ir_volt = np.round(wps1.hv305.read_pv.get())
+    # # print_to_gui('WARNING VOLTAGES ARE NOT BEING READ FOR METADATA')
+    # # i0_volt = 0
+    # # it_volt = 0
+    # # ir_volt = 0
+    #
+    # md = {'nslsii_current' : nsls_ii.beam_current.get(),
+    #       'nslsii_status' : nsls_ii.return_status_string(),
+    #       'nslsii_energy' : nsls_ii.energy_str,
+    #       'pulses_per_degree': hhm.pulses_per_deg,
+    #       'angle_offset': str(hhm.angle_offset.get()),
+    #       'angle_offset_deg': f'{np.round(hhm.angle_offset.get() * 180 / np.pi, 3)} deg',
+    #       'mono_encoder_resolution': str(np.round(hhm.main_motor_res.get() * np.pi / 180 * 1e9)) + ' nrad',
+    #       'mono_scan_mode': 'pseudo-channel cut',
+    #       'harmonic_rejection' : hhrm.current_stripe,
+    #       'sample_stage': 'ISS.giant_xy stage',
+    #       'sample_x_position': giantxy.x.user_readback.get(),
+    #       'sample_y_position': giantxy.y.user_readback.get(),
+    #       'i0_par' : f'17 cm, He: {gas_he_perc}%, N2: {gas_n2_perc}%',
+    #       'it_par' : f'30 cm, He: {gas_he_perc}%, N2: {gas_n2_perc}%',
+    #       'ir_par' : f'17 cm, He: {gas_he_perc}%, N2: {gas_n2_perc}%',
+    #       'iff_par' : f'PIPS',
+    #       'i0_volt' : i0_volt,
+    #       'it_volt' : it_volt,
+    #       'ir_volt' : ir_volt,
+    #       'i0_gain' : i0_amp.get_gain()[0],
+    #       'it_gain' : it_amp.get_gain()[0],
+    #       'ir_gain' : ir_amp.get_gain()[0],
+    #       'iff_gain' : iff_amp.get_gain()[0],
+    #       }
+    md = get_standard_metadata()
 
     for indx in range(8):
         md[f'ch{indx+1}_offset'] = getattr(apb, f'ch{indx+1}_offset').get()
@@ -81,7 +131,6 @@ def get_general_md():
             md[f'ch{indx+1}_amp_gain']= amp.get_gain()[0]
         else:
             md[f'ch{indx+1}_amp_gain'] = 0
-
 
     sample_metadata = get_standard_metadata()
 
