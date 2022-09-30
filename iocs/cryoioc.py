e2986066ec9eab86de872057532c2974751c9d0b
Denis Leshchev <leshchev.denis@gmail.com>
2022-09-30 15:27:38 -0400

    added a cryocooler watcher IOC that is supposed to prevent LN2 bath overfill due to a malfunctioning valve.

---
+++ iocs/cryoioc.py
@@ -0,0 +1,69 @@
+from textwrap import dedent
+
+from caproto import ChannelType
+from caproto._dbr import _LongStringChannelType
+from caproto.server import PVGroup, ioc_arg_parser, pvproperty, run
+import time as ttime
+import numpy as np
+from ophyd import Component as Cpt, Device, EpicsSignalRO, Kind
+from nslsii.devices import TwoButtonShutter
+
+cryocooler_ps_level = EpicsSignalRO('XF:08IDA-UT{Cryo:1}PS-I', name='cryocooler_ps_level')
+cryocooler_bath_level = EpicsSignalRO('XF:08IDA-UT{Cryo:1}LN2Bath-I', name='cryocooler_bath_level')
+cryocooler_valve = TwoButtonShutter('XF:08IDA-UT{Cryo:1-Vlv}', name='cryocooler_valve')
+
+cryocooler_bath_level_lo = 60
+cryocooler_bath_level_hi = 70
+
+
+print('#####################################################################')
+print('###                    LAUNCHING CRYO WATCH IOC                   ###')
+print('#####################################################################')
+
+
+class CryoIOC(PVGroup):
+    """
+    An IOC for watching the ISS cryostat
+
+    """
+    heartbeat = pvproperty(
+        value=0,
+        doc='IOC heartbeat'
+    )
+
+    dwell_time = 5 # seconds
+
+    @heartbeat.startup
+    async def heartbeat(self, instance, async_lib):
+
+        while True:
+            await async_lib.sleep(self.dwell_time)
+            await instance.write(value=int(not instance.value))
+            try:
+                cryocooler_bath_level_value = cryocooler_bath_level.get()
+                print(f'{cryocooler_bath_level_value=:.3f}.', end=' ')
+
+                if (cryocooler_bath_level_value > cryocooler_bath_level_hi) and (cryocooler_valve.status.get() == 'Open'):
+                    print(f'REACHED THE HIGHER THRESHOLD. CLOSING THE VALVE.')
+                    st = cryocooler_valve.set('Close')
+                    st.wait()
+
+                elif (cryocooler_bath_level_value <= cryocooler_bath_level_lo) and (cryocooler_valve.status.get() == 'Close'):
+                    print('REACHED THE LOWER THRESHOLD. OPENING THE VALVE.')
+                    st = cryocooler_valve.set('Open')
+                    st.wait()
+
+                else:
+                    print(f'NORMAL OPERATIONS.')
+            except Exception as e:
+                print(f'Failed to act on the valve. Reason: {e}')
+
+
+
+
+if __name__ == '__main__':
+    ioc_options, run_options = ioc_arg_parser(
+        default_prefix='XF:08IDA-CryoWatch:',
+        desc=dedent(CryoIOC.__doc__))
+    ioc = CryoIOC(**ioc_options)
+    run(ioc.pvdb, **run_options)
