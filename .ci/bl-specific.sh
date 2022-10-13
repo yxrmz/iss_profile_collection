839d490fe7f2ba62fb1f74e8abb886e4fdcb63db
Maksim Rakitin <mrakitin@bnl.gov>
2022-10-13 13:53:24 -0400

    CI: add `iss-local.yml` databroker config

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -7,6 +7,9 @@ pip install -vv git+https://github.com/NSLS-II-ISS/xas@main
 pip install -vv git+https://github.com/NSLS-II-ISS/isscloudtools@master "oauth2client<4.0.0"
 pip install -vv git+https://github.com/NSLS-II-ISS/xview@main
 
+# Add a non-standard 'iss-local.yml' databroker config:
+cp -v $HOME/.config/databroker/iss.yml $HOME/.config/databroker/iss-local.yml
+
 # Create non-standard directories:
 sudo mkdir -v -p /nsls2/xf08id/metadata/
 
